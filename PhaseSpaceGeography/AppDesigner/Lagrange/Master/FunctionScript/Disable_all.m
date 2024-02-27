b55f8310a5d001b2ee875df8f6531cf4950e76f7
BrunoCordani <152741460+BrunoCordani@users.noreply.github.com>
2024-02-27 23:14:52 +0100

    Delete PhaseSpaceGeography/AppDesigner/Lagrange directory

--- PhaseSpaceGeography/AppDesigner/Lagrange/Master/FunctionScript/Disable_all.m
+++
@@ -1,6 +0,0 @@
-set(app.pushbutton_Orbits, 'Enable', 'off')
-set(app.pushbutton_AA, 'Enable', 'off')
-set(app.pushbutton_error, 'Enable', 'off')
-set(app.pushbutton_freq, 'Enable', 'off')
-set(app.pushbutton_level, 'Enable', 'off')
-set(app.pushbutton_FMI, 'Enable', 'off')
